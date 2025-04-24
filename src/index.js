d9d0e4509df639b1c9aa687e9406ba5f67443ede
MLubeckis <185762906+MLubeckis@users.noreply.github.com>
2025-04-24 14:13:55 +0300

    Add fetchAddress to get adress api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-import './asset/css/main.css';
-import './asset/css/mayer_reset.css';
-console.log('Hello World!')
+import "./asset/css/main.css";
+import "./asset/css/mayer_reset.css";
+import { getAddresses, updateSelection } from "./models/fetchAddress";
+console.log("Hello World!");
+
+document
+    .querySelector(".location-search input")
+    .addEventListener("focus", () => {
+        document.querySelector(".dropdown-addreses").classList.toggle("show");
+    });
+
+// document.querySelector('.location-search input').addEventListener('focusout', () => {
+//     document.querySelector('.dropdown-addreses').classList.toggle('show');
+// })
+
+let time = null;
+document
+    .querySelector(".location-search input")
+    .addEventListener("keyup", (e) => {
+        const div = document.querySelector('.dropdown-addreses');
+        const address = e.target.value;
+        clearTimeout(time);
+        time = setTimeout(() => {
+            updateSelection(div, address);
+        }, 600);
+    });
+
+window.addEventListener("mousedown", (e) => {
+    const addressInputField = document.querySelector(".location-search input");
+    // console.log(document.querySelector('.dropdown-addreses').classList);
+    if (e.target.classList == "dd") {
+        addressInputField.value =
+            e.target.textContent;
+        document.querySelector(".dropdown-addreses").classList.toggle("show");
+    } else if(e.target != addressInputField){
+        document.querySelector(".dropdown-addreses").classList.remove("show");
+    }
+});
---
+++ src/models/fetchAddress.js
@@ -0,0 +1,24 @@
+export async function getAddresses(address) {
+    let response = await fetch(
+        `https://api.kartes.lv/v3/KVDM_QHL5O/search?q=${address}&layers=adrese&fields=name,pop_place,iela,maja,terit_vien,admin_vien,valsts,&limit=4`
+    );
+    let addressesObj = await response.json();
+    return addressesObj;
+}
+
+export async function updateSelection(div, address) {
+    const addressesObj = await getAddresses(address);
+    const addressesArr = addressesObj.adrese;
+    div.innerHTML = "";
+    if (addressesArr.length > 0){
+        for (address of addressesArr) {
+            const selection = document.createElement("div");
+            selection.classList.add("dd");
+            selection.textContent = `${address.iela || address.terit_vien || ""} ${address.maja || ""}, ${
+                address.pop_place || address.admin_vien || ""
+            }`;
+            div.append(selection);
+        }
+    }
+    }
+    
