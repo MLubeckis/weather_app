511d20174247383fc2380187e0b145050514e859
unknown <185762906+MLubeckis@users.noreply.github.co>
2025-04-27 20:04:52 +0300

    Add AI created pics + install Embla Carousel

--- src/models/fetchWeather.js
+++ src/models/fetchWeather.js
@@ -1,6 +1,6 @@
-import { responseObj } from "./response";
+import { responseObj as response }  from "./response";
 export async function getWeatherData(){
     // const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Lokomotives%20iela%2034/next7days/next15days?unitGroup=metric&key=9GWHNNZ3R86XP9D3V2MW5QHVE&contentType=json');
     // const data = await response.json();
-    console.log(responseObj);
+    console.log(response);
 }
